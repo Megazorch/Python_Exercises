e9140505e24b44af3b4dfbb4c8b4ff9ce024f22e
Roman Ivakhiv <megazorch@gmail.com>
2022-12-30 23:01:22 +0200

    Testing UUID generating v4 function in Postgres.

--- person-car.sql
+++ person-car.sql
@@ -1,25 +1,26 @@
 create table car (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
+	car_uid UUID NOT NULL PRIMARY KEY,
 	make VARCHAR(100) NOT NULL,
 	model VARCHAR(100) NOT NULL,
 	price NUMERIC(19, 2) NOT NULL
 );
 
 create table person (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
+	person_uid UUID NOT NULL PRIMARY KEY,
 	first_name VARCHAR(50)	NOT NULL,
 	last_name VARCHAR(50)	NOT NULL,
     gender VARCHAR(15) NOT NULL,
 	email VARCHAR(100),
 	date_of_birth DATE	NOT NULL,
 	country_of_birth VARCHAR(50) NOT NULL,
-    car_id BIGINT REFERENCES car (id),
-    UNIQUE(car_id)
+    car_uid UUID REFERENCES car (car_uid),
+    UNIQUE(car_uid),
+	UNIQUE(email)
 );
 
-insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Ethe', 'Ardern', 'eardern0@hibu.com', 'Male', '2022-06-18', 'Philippines');
-insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Ignacio', 'Charleston', 'icharleston1@ameblo.jp', 'Male', '2022-08-19', 'Egypt');
-insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Dolph', 'Miko', 'dmiko2@youku.com', 'Male', '2021-12-30', 'China');
+insert into person (person_uid, first_name, last_name, email, gender, date_of_birth, country_of_birth) values (uuid_generate_v4(), 'Ethe', 'Ardern', 'eardern0@hibu.com', 'Male', '2022-06-18', 'Philippines');
+insert into person (person_uid, first_name, last_name, email, gender, date_of_birth, country_of_birth) values (uuid_generate_v4(), 'Ignacio', 'Charleston', 'icharleston1@ameblo.jp', 'Male', '2022-08-19', 'Egypt');
+insert into person (person_uid, first_name, last_name, email, gender, date_of_birth, country_of_birth) values (uuid_generate_v4(), 'Dolph', 'Miko', 'dmiko2@youku.com', 'Male', '2021-12-30', 'China');
 
-insert into car (id, make, model, price) values (1, 'Ford', 'Festiva', '71127.50');
-insert into car (id, make, model, price) values (2, 'Nissan', 'Altima', '48611.06');
+insert into car (car_uid, make, model, price) values (uuid_generate_v4(), 'Ford', 'Festiva', '71127.50');
+insert into car (car_uid, make, model, price) values (uuid_generate_v4(), 'Nissan', 'Altima', '48611.06');
